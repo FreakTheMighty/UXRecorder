7d5f5d6910790aa8cbc18a94420976ac6c0532c5
Jesse Vander Does <jvanderdoes@gmail.com>
2015-11-14 16:11:30 -0800

    Fixing tab event mix up issue

--- src/lib/background.js
+++ src/lib/background.js
@@ -1,23 +1,23 @@
-var latestImage;
+var latestImages = {};
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if (request.command === 'screenGrab') {
       chrome.tabs.captureVisibleTab(sender.tab.windowId, {format: 'jpeg', quality: 90}, function (image) {
          // You can add that image HTML5 canvas, or Element.
-        latestImage = image;
-        console.log(request);
+        latestImages[sender.tab.windowId] = image;
       });
-    } else if (request.command === 'log' && latestImage ) {
+    } else if (request.command === 'log' && latestImages[sender.tab.windowId] ) {
       $.ajax({
         url: 'http://localhost:3000/save', 
         data: JSON.stringify({
-          image: latestImage,
+          image: latestImages[sender.tab.windowId],
           data: request
         }),
         type: 'POST',
         contentType : 'application/json',
       });
+      delete latestImages[sender.tab.windowId];
     }
   }
 );
